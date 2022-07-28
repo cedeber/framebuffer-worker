b4e678f13d0f51a13f58966ef14a583c47d362cd
Cédric Eberhardt <hello+code@cedeber.fr>
2022-07-29 00:37:17 +0200

    slow drawing test

--- main.js
+++ main.js
@@ -20,20 +20,24 @@ requestAnimationFrame(animate);
 const canvas = document.getElementById("canvas");
 
 init(canvas).then(async ({ reset, paint, line }) => {
-	const draw = async () => {
-		await reset();
-		await line(0, 0, canvas.width, canvas.height, new Color(255, 255, 255), 1);
-		await line(10, 10, 30, 40, new Color(255, 255, 255), 7);
-		await line(50, 80, 270, 40, new Color(255, 255, 255), 3);
+	const draw = async (i = 0) => {
+		await line(0 + i, 30, canvas.width, canvas.height, new Color(255, 255, 255), 1);
+		await line(10 + i, 60, 30, 40, new Color(255, 255, 255), 7);
+		await line(50 + i, 110, 270, 40, new Color(255, 255, 255), 3);
 	};
 
+	await reset();
 	await draw();
 	await paint();
 
 	const cb = async (event) => {
-		await draw();
-		await line(event.offsetX, 0, event.offsetX, canvas.height);
-		await line(0, event.offsetY, canvas.width, event.offsetY);
+		const random = Math.floor(Math.random() * 25);
+		await reset();
+		for (let i = random; i < 400 + random; i++) {
+			await draw(i);
+			await line(event.offsetX, 0, event.offsetX, canvas.height);
+			await line(0, event.offsetY, canvas.width, event.offsetY);
+		}
 		await paint();
 	};
 
