fd8ff748daeb5e27ebb0f838a5a5985f70834559
Cédric Eberhardt <hello+code@cedeber.fr>
2022-08-06 12:01:55 +0200

    update / optimize crates

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,2 +1,3 @@
 cargo build --lib --release --target wasm32-unknown-unknown
 wasm-bindgen --target web --out-dir lib/wasm target/wasm32-unknown-unknown/release/canvas.wasm
+wasm-opt -Os lib/wasm/canvas_bg.wasm -o lib/wasm/canvas_bg.wasm
