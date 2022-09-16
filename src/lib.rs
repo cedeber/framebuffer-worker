3904325d5472bb87e7fce8d3470c683365bcca4c
Cédric Eberhardt <hello+code@cedeber.fr>
2022-09-16 22:04:25 +0200

    Remove wee_alloc, unmaintained and not a lot lighter than the default Rust standard allocator.

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,6 @@ use profont::{
 use serde::de::DeserializeOwned;
 use wasm_bindgen::prelude::*;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen(start)]
 pub fn main_wasm() -> Result<(), JsValue> {
 	console_error_panic_hook::set_once();
